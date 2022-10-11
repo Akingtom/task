5ff7ce28e8816c2196e10c94f62e12941f45d3aa
Chuggy <victomilola@gmail.com>
2022-10-11 11:52:56 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,16 @@
+
+#!bin/bash
+Sudo apt-get install figlet
+Figlet Tomilola
+mkdir compare $$ cd compare
+Wget (https://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz)
+gunzip unix_intro_data.tar.gz
+tar -xf unix_intro_data.tar
+cd seqmonk_genomes, cd' Saccharomyces cerevisiae', cd EF4, less rRNAs Mito.dat
+cp Mito.dat ../../../
+nano Mito.dat, ctrl + o, ctrl x
+nano Mito.dat ctrl o, Mitochondrion.txt, ctrl x
+wc -l  lane8_DD_P4_TTAGGC_L008_R1.fastq.gz
+wc -l *.fastq.gz
+touch fasta.txt
+wc -l *.fastq.gz > fasta.txt
